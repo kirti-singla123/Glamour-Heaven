23e8ea6ab3250b251c60a0e8f7a4d87a5e300e1b
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-14 15:24:51 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/hero-image.jpg";
+import heroImage from "@/assets/hero-image1.jpg";
 
 const Hero = () => {
   return (
