444a2801fbf4db5d469f79314ef354780419f435
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-19 10:49:06 +0530

    Update BookingForm.tsx

--- src/pages/BookingForm.tsx
+++ src/pages/BookingForm.tsx
@@ -18,7 +18,7 @@ function BookingForm() {
     const bookingData = { ...form, service: serviceName };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/bookings/", {
+      const response = await fetch("https://glamourheaven-backend.onrender.com/api/bookings/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
