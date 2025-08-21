a4b875900cee15c24c79044e1293f43504040325
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-21 16:16:44 +0530

    Update DashboardLogin.tsx

--- src/pages/DashboardLogin.tsx
+++ src/pages/DashboardLogin.tsx
@@ -21,7 +21,7 @@ function DashboardLogin() {
     <div
       className="flex items-center justify-center min-h-screen bg-cover bg-center"
       style={{
-        backgroundImage: "url('https://i.etsystatic.com/43735431/r/il/38bf79/5940012618/il_340x270.5940012618_ij50.jpg')", // <-- replace with your URL
+        backgroundImage: "url('https://wallpapers.com/images/featured/hair-salon-37ytdz60uxhrf8mv.jpg')", // <-- replace with your URL
       }}
     >
       <div className="p-10 bg-white/90 shadow-2xl rounded-2xl w-full max-w-md">
